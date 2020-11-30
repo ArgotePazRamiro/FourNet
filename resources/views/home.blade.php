da2b3951fbc934ea0b7d315deead420416fc873b
aqm1290 <aqm1290@gmail.com>
2020-11-30 02:32:49 -0400

    Cambio2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-
-    <div
-      class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-      <h1 class="h2">Inicio</h1>
-      <div class="btn-toolbar mb-2 mb-md-0">
-      
-      </div>
-    </div>
-
-
+<main class="sm:container sm:mx-auto sm:mt-10">
+    
+</main>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+Auth::routes();
+
+Route::resource("clientes", clientes::class);
