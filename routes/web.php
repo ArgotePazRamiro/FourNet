0fd1321308da24e8436361a6843373bf9dcd6dd3
aqm1290 <aqm1290@gmail.com>
2020-12-01 19:59:18 -0400

    asdasd

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,7 @@ Route::resource('/clientes', clientes::class);
 
 Auth::routes(); 
 
+Auth::routes();
+
+Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
